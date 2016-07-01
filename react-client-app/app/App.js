e4a7ed4e8289264903702b208e37da5f7fee0c6c
meta <metaengineering@tuta.io>
2016-07-01 18:11:40 -0400

    text labels are now moving reliably according to player position

--- react-client-app/app/App.js
+++ react-client-app/app/App.js
@@ -9,6 +9,7 @@ import PIXI from '../vendor/phaser/pixi.js';
  
  var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create, update: update });
  	  var t;
+ 	  var actors = {};
       function create() {
           this.store = store.getState();
          
@@ -18,13 +19,21 @@ import PIXI from '../vendor/phaser/pixi.js';
           t = game.add.text(game.world.centerX-300, 0, text, style);
   		}
   		function update() {
-  			console.log(t);
+  			
   			this.store = store.getState();
-			 console.log("GAME STORE:");
-			 console.log(this.store);
+			
 			 if (this.store.actors.length > 0) {
-			 	var text = "- phaser -\n with a sprinkle of \n pixi dust."+this.store.actors[0].name;
-			 t.text = text;
+			 	var style = { font: "65px Arial", fill: "#ff0044", align: "center" };
+			 	for (var actor of this.store.actors) {
+			 		if (!actors[actor.name]) {
+			 			actors[actor.name] = game.add.text(game.world.centerX+actor.posX, game.world.centerY+actor.posY, actor.name, style);
+			 		}
+			 		else {
+			 			actors[actor.name].x = game.world.centerX+actor.posX;
+			 			actors[actor.name].y = game.world.centerY+actor.posY;
+			 		}
+			 	}
+			 t.destroy();
 			 }
 			 
   		}
